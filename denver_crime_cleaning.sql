fba721518b550f2d99de8528ab747bb603d37364
Santiago Castillo <scastillo@isengard.mines.edu>
2023-12-04 09:37:50 -0700

    made slight modifications

--- denver_crime_cleaning.sql
+++ denver_crime_cleaning.sql
@@ -2,7 +2,10 @@ SET search_path = f23_group20, scastillo;
 
 -- Seeing which incident_id elements are repeated
 
-SELECT incident_id, COUNT(*)
+SELECT incident_id, COUNT(*) AS incident_count
 FROM denver_demographics
 GROUP BY incident_id
-HAVING COUNT(*) > 1;
+HAVING COUNT(*) > 1
+ORDER BY incident_count DESC;
+
+
