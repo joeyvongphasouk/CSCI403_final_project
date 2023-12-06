a7eebc38d1f02037efc9452df04fb9bfaec553fc
Santiago Castillo <scastillo@isengard.mines.edu>
2023-12-05 17:16:49 -0700

    tried to load table, issue with null values

--- denver_crime.sql
+++ denver_crime.sql
@@ -1,1 +1,29 @@
--- more cool stuff below
+SET search_path = f23_group20;
+
+
+DROP TABLE IF EXISTS denver_crime CASCADE;
+
+CREATE TABLE denver_crime (
+    incident_id NUMERIC,
+    offense_id NUMERIC,
+    offense_code NUMERIC,
+    offense_code_extension NUMERIC,
+    offense_type_id TEXT,
+    offense_category_id TEXT,
+    first_occurrence_date TIMESTAMP,
+    last_occurrence_date TEXT,
+    reported_date TIMESTAMP,
+    incident_address TEXT,
+    geo_x TEXT,
+    geo_y TEXT,
+    geo_lon NUMERIC,
+    geo_lat NUMERIC,
+    district_id TEXT,
+    precinct_id NUMERIC,
+    neighborhood_id TEXT,
+    is_crime NUMERIC,
+    is_traffic NUMERIC,
+    victim_count NUMERIC
+);
+
+\COPY denver_crime FROM 'crime2021.csv' WITH CSV HEADER;
