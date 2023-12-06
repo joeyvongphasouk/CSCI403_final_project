03ce87f1d57c2e4c3edcb17df0eaf1e774a61962
Santiago Castillo <scastillo@isengard.mines.edu>
2023-12-06 08:22:51 -0700

    added an epic query

---
+++ project_queries.sql
@@ -0,0 +1,18 @@
+SET search_path = f23_group20
+
+-- SHARED ATTRIBUTES:
+-- district_id,
+-- precinct_id,
+-- neighborhood_id,
+
+-- incident_id
+-- first_occurrence_date
+
+-- joining on a table from both datasets
+SELECT c.incident_id, c.victim_count, t.tu1_vehicle_type, t.tu2_vehicle_type 
+FROM denver_crime_incident c
+JOIN denver_traffic_incident t ON c.incident_id = t.incident_id
+ORDER BY victim_count DESC;
+
+
+
