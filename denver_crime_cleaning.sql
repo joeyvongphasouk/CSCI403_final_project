22acd662e7b3347a8465418f129030a4319aacdb
Santiago Castillo <scastillo@isengard.mines.edu>
2023-12-05 18:16:52 -0700

    determined key for denver_crime

--- denver_crime_cleaning.sql
+++ denver_crime_cleaning.sql
@@ -1,17 +1,32 @@
 SET search_path = f23_group20;
 
 -- Seeing which incident_id elements are repeated
-
 SELECT incident_id, COUNT(*) AS incident_count
 FROM denver_crime
 GROUP BY incident_id
 HAVING COUNT(*) > 1
 ORDER BY incident_count DESC;
 
+-- RESULTS
 -- there are a couple of incident_id elements that are repeated at most
 -- 6 times, this will need to be adressed in normalization
 
--- Seeing which 
 
+-- Seeing which offense_id elements are repeated
+SELECT offense_id, COUNT(*) AS offense_count
+FROM denver_crime
+GROUP BY offense_id
+HAVING COUNT(*) > 1
+ORDER BY offense_count DESC;
+
+-- RESULTS
+-- no repeated elements, could use this as a primary key
+
+-- Seeing which offense_code elements are repeated
+SELECT offense_code, COUNT(*) AS offense_code_count
+FROM denver_crime
+GROUP BY offense_code
+HAVING COUNT(*) > 1
+ORDER BY offense_code_count DESC;
 
 
